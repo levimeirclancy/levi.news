bb7dc43b7e0944e942ced4b2667fe31f87d38e27
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2024-12-23 04:22:49 -0800

    Update index.php

--- index.php
+++ index.php
@@ -18,16 +18,18 @@ echo "Professional links";
 echo "<br>";
 echo "— <a href='http://linkedin.com/in/levimeirclancy/'>LinkedIn</a>";
 echo "<br>";
-echo "— <a href='https://levi.pictures'>Photography</a>";
-echo "<br>";
 echo "— <a href='https://levi.pictures'>Blogs @ Times of Israel</a>";
 
 
+echo "Get to know me";
 echo "<br>";
+echo "About &rarr; <a href='https://levi.news'>levi.news</a>";
 echo "<br>";
-echo "Contact";
+echo "Photography &rarr; <a href='https://levi.pictures'>levi.pictures</a>";
 echo "<br>";
-echo "<a href='mailto:info@levi.news'>info@levi.news</a>";
+echo "Education &rarr; <a href='https://conversationpieces.rsvp'>conversationpieces.rsvp</a>";
+echo "<br>";
+echo "Email &rarr; <a href='mailto:info@levi.news'>info@levi.news</a>";
 
 echo "<br>";
 echo "<br>";
