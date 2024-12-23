caa98210fcc4849f8d5663910ca260f9e2490638
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2024-12-23 04:02:30 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?
+
+include_once("index-content.php");
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+echo "לוי מאיר קלנסי";
+echo "<br>";
+echo "Levi Meir Clancy";
+
+echo "<br>";
+echo "<br>";
+
+echo "Professional links";
+echo "<br>";
+echo "— <a href='http://linkedin.com/in/levimeirclancy/'>LinkedIn</a>";
+echo "<br>";
+echo "— <a href='https://levi.pictures'>Photography</a>";
+echo "<br>";
+echo "— <a href='https://levi.pictures'>Blogs @ Times of Israel</a>";
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+
+?>
