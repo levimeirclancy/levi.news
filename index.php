3c26e969aaec39d453111521bb5d6c449af921e4
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2024-12-23 21:43:38 -0800

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,23 @@
 
 include_once("index-content.php");
 
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+echo "<style>";
+	// css
+	echo "</style>";
+echo "</head>";
+echo "<body>";
+
 echo "<div id='body-case'>";
 
-  echo "<span id='header-padding'></span>";
-	echo "<span id='header-he'>לוי<br>מאיר<br>קלנסי</span>";
+echo "<span class='header-padding'></span>";
+
+echo "<span id='header-he'>לוי<br>מאיר<br>קלנסי</span>";
 echo "<span id='header-en'>Levi<br>Meir<br>Clancy</span>";
 
-echo "<span id='body-spacer'></span>";
+echo "<span class='body-spacer'></span>";
 
 echo "Professional links";
 echo "<br>";
@@ -16,8 +26,7 @@ echo "— <a href='http://linkedin.com/in/levimeirclancy/'>LinkedIn</a>";
 echo "<br>";
 echo "— <a href='https://blogs.timesofisrael.com/author/levi-clancy/'>Blogs @ Times of Israel</a>";
 
-echo "<br>";
-echo "<br>";
+echo "<span class='body-spacer'></span>";
 
 echo "Get to know me";
 echo "<br>";
@@ -29,10 +38,11 @@ echo "Education &rarr; <a href='https://conversationpieces.rsvp'>conversationpie
 echo "<br>";
 echo "Email &rarr; <a href='mailto:info@levi.news'>info@levi.news</a>";
 
+echo "<span class='footer-padding'></span>";
+
 echo "</div>";
 
-echo "<span id='header-padding'></span>";
-
-
+echo "</body>";
+echo "</html>";
 
 ?>
