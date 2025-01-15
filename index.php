e6ed29c981946762766e92bed30aeae250918191
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-01-15 10:48:53 -0800

    Update index.php

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ echo "<style>";
 	echo ".section-wrapper-blue { background: rgba(".rgb_formatter($rgb_blue).",1); }";
 	echo ".section-wrapper-blue .section-subheader { background: rgba(".rgb_formatter($rgb_blue,-20).",1); }";
 
-	echo ".section-subheader { font-family: Verdana; background: inherit; font-size: 180%; display: inline-block; font-weight: 700; line-height: 0.9; padding: 30px 40px; margin: -90px auto 40px; border-radius: 50px 50px 50px 0; } ";
+	echo ".section-subheader { font-family: Verdana; border: 3px solid #f0f0f0; background: inherit; font-size: 180%; display: inline-block; font-weight: 700; line-height: 0.9; padding: 30px 40px; margin: -90px auto 40px; border-radius: 50px 50px 50px 0; } ";
 	echo ".section-subheader-rotate-ccw { transform: rotate(-5deg); }";
 	echo ".section-subheader-rotate-cw { transform: rotate(5deg); }";
 	echo ".section-description { font-family: Verdana; font-size: 120%; display: block; margin: 0 auto; font-weight: 400; text-align: left; } ";
