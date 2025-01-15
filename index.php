8e442b969ad96fc78840c775fa796441ad1bc65e
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-01-15 09:41:00 -0800

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,13 @@ echo "<style>";
 echo "</head>";
 echo "<body>";
 
+// Name-ful header
+
 echo "<span class='body-spacing'></span>";
+echo "<span class='body-spacing'></span>";
+
+echo "<div class='header-bio width-wrapper'>My name is ...</div>";
+
 echo "<span class='body-spacing'></span>";
 
 echo "<div class='header-he'>לוי מאיר<br>קלנסי</div>";
@@ -50,44 +56,61 @@ echo "<div class='header-ku'>لیڤی مایر<br>کلنسی</div>";
 
 echo "<span class='body-spacing'></span>";
 
-echo "<div class='header-bio width-wrapper'><b>Levi Meir Clancy is a photographer, videographer, and public speaker.</b> Shaped by his Jewish, Ryukyuan (Indigenous Japanese), and LGBTQ experience, his work explores power dynamics, context, and identity. His professional background in history, communications, and advocacy — from Iraq to the Bay Area — is poured into creating impactful visual stories, educational materials, and in-person experiences.</div>";
+// Intro summary
+
+echo "<div class='header-bio width-wrapper'>I am a direct support professional, a media generalist, and an all-around hard worker.</div>";
 
 echo "<span class='body-spacing'></span>";
 
-echo "<div class='section-info section-orange'>";
-echo "<div class='section-subheader width-wrapper'>Passion and purpose</div>";
-echo "<div class='section-description width-wrapper'>
-I tell stories through photos, videos, and events. My work explores Jewishness, identity, personal history, and cross-cultural connections. Rooted in my Jewish values, I also support adults with disabilities in building independence and community.</div>";
-echo "</div>";
-
-echo "<a href='http://linkedin.com/in/levimeirclancy/'><div class='link-bubble'>LinkedIn</div></a>";
-echo "<a href='https://blogs.timesofisrael.com/author/levi-clancy/'><div class='link-bubble'>Blogs @ Times of Israel</div></a>";
-
-echo "<span class='body-spacing'></span>";
-
-echo "<div class='section-info section-pink'>";
-echo "<div class='section-subheader width-wrapper'>Public speaking</div>";
-echo "<div class='section-description width-wrapper'>I am a seasoned public speaker who has delivered talks in prestigious settings such as TEDxDuhok, AMP Conf, GSMA Mobile 360, and Qesher. My presentations focus on Jewish heritage, cultural advocacy, and fostering cross-cultural understanding, drawing from my extensive lived experiences with varied identites and geographies.</div>";
-echo "</div>";
-
-echo "<a href='https://www.youtube.com/watch?v=n050dgtyOck'><div class='link-bubble'>2017 Oct 10 &rarr; TEDxDuhok</div></a>";
-echo "<a href='https://www.youtube.com/watch?v=ptyGKlmVniE&t=8s'><div class='link-bubble'>2021 Mar 23 &rarr; HARIF</div></a>";
-
-echo "<span class='body-spacing'></span>";
-
-echo "<div class='section-info section-blue'>";
-echo "<div class='section-subheader width-wrapper'>Get to know me</div>";
-echo "<div class='section-description width-wrapper'>I am...</div>";
-echo "</div>";
-
-echo "<a href='https://levi.news'><div class='link-bubble'>About &rarr; levi.news</div></a>";
-echo "<a href='https://levi.pictures'><div class='link-bubble'>Photography &rarr; levi.pictures</div></a>";
-echo "<a href='https://conversationpieces.rsvp'><div class='link-bubble'>Education &rarr; conversationpieces.rsvp</div></a>";
+// echo "<a href='https://levi.news'><div class='link-bubble'>About &rarr; levi.news</div></a>";
+echo "<a href='https://levi.pictures'><div class='link-bubble'>Photography and videography &rarr; levi.pictures</div></a>";
+echo "<a href='https://ziolicio.us'><div class='link-bubble'>Identity building &rarr; ziolicio.us</div></a>";
+echo "<a href='https://conversationpieces.rsvp'><div class='link-bubble'>Education resources &rarr; conversationpieces.rsvp</div></a>";
+echo "<a href='https://blogs.timesofisrael.com/author/levi-clancy/'><div class='link-bubble'>Blog &rarr; Times of Israel</div></a>";
 echo "<a href='mailto:info@levi.news'><div class='link-bubble'>Email &rarr; info@levi.news</div></a>";
 
 echo "<span class='body-spacing'></span>";
+
+// Disability services
+
+echo "<div class='section-info section-orange'>";
+echo "<div class='section-subheader width-wrapper'>Growth and Compassion</div>";
+echo "<div class='section-description width-wrapper'>";
+echo "As a <b>direct support professional</b>, I support adults with disabilities in building independence and community. Through community-level and one-on-one activities, my role helps disabled adults achieve growth and independence through strategic challenges, personalized coaching, and creative experiences tailored to their unique intellectual disabilities. My work is set in a neurodiverse <q>urban village</q> of disabled and non-disabled adults based on Kibbutz Kishorit, an innovative Israeli program rooted in Jewish values.";
+echo "</div>";
+
 echo "<span class='body-spacing'></span>";
+
+echo "<a href='http://linkedin.com/in/levimeirclancy/'><div class='link-bubble'>LinkedIn</div></a>";
+
 echo "<span class='body-spacing'></span>";
+
+// Public speaking
+
+echo "<div class='section-info section-pink'>";
+echo "<div class='section-subheader width-wrapper'>Impactful speaker</div>";
+echo "<div class='section-description width-wrapper'>My public speaking ranges from walking tours to large auditoriums, with experience including TEDxDuhok, AMP Conf, GSMA Mobile 360, Qesher, and interviews on radio, podcasts, and television.</div>";
+echo "</div>";
+
+echo "<a href='https://www.youtube.com/watch?v=n050dgtyOck'><div class='link-bubble'>2017 &rarr; TEDxDuhok</div></a>"; // 2017 Oct 10
+echo "<a href='https://www.youtube.com/watch?v=ptyGKlmVniE'><div class='link-bubble'>2021 &rarr; HARIF</div></a>"; // 2021 Mar 23
+echo "<a href='https://www.youtube.com/watch?v=BrDBsS1Sko0'><div class='link-bubble'>2019 &rarr; HARIF</div></a>"; // 
+
+echo "<span class='body-spacing'></span>";
+
+// Publishing
+
+echo "<div class='section-info section-gray'>";
+echo "<div class='section-subheader width-wrapper'>Publishing credits</div>";
+echo "<div class='section-description width-wrapper'>I am...</div>";
+echo "</div>";
+
+echo "<a href='https://www.futureofjewish.com/p/the-little-known-story-of-the-druze'><div class='link-bubble'>2024 Dec 26 &rarr; Future of Jewish</div></a>";
+echo "<a href='https://jewishunpacked.com/shine-a-light-on-antisemitism-this-hanukkah-jewish-survival-depends-on-it/'><div class='link-bubble'>2024 Dec 25 &rarr; Jewish Unpacked</div></a>";
+
+echo "<span class='body-spacing'></span>";
+
+// Coverage
 
 echo "<div class='section-info section-gray'>";
 echo "<div class='section-subheader width-wrapper'>Press</div>";
@@ -100,19 +123,18 @@ echo "<a href='https://forward.com/news/473198/in-iraqi-kurdistan-a-one-man-muse
 
 echo "<span class='body-spacing'></span>";
 
-echo "<div class='section-info section-gray'>";
-echo "<div class='section-subheader width-wrapper'>Credits</div>";
-echo "<div class='section-description width-wrapper'>I am...</div>";
-echo "</div>";
-
-echo "<a href='https://www.futureofjewish.com/p/the-little-known-story-of-the-druze'><div class='link-bubble'>2024 Dec 26 &rarr; Future of Jewish</div></a>";
-echo "<a href='https://jewishunpacked.com/shine-a-light-on-antisemitism-this-hanukkah-jewish-survival-depends-on-it/'><div class='link-bubble'>2024 Dec 25 &rarr; Jewish Unpacked</div></a>";
+// Footer
 
 // echo " <a href='___'>___ &rarr; ___</div></a>";
 
 echo "<span class='body-spacing'></span>";
+echo "<span class='body-spacing'></span>";
+echo "<span class='body-spacing'></span>";
 
-
+echo "<div class='section-info section-blue'>";
+echo "<div class='section-subheader width-wrapper'>Get to know me</div>";
+echo "<div class='section-description width-wrapper'>Send me an <a href='mailto:info@levi.news'>email</a> to be in touch.</div>";
+echo "</div>";
 
 echo "</body>";
 echo "</html>";
